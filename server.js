10f08c1c677be2f3d16308cc4cfa91a90787b54c
DeadInsane5 <aditya.ajaykumar.2006@gmail.com>
2025-06-15 09:46:42 +0530

    initial commit; finished logic; adding styles

---
+++ server.js
@@ -0,0 +1,12 @@
+// server.js
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Serve static files (HTML, JS, CSS)
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000, () => {
+  console.log('Server running at http://localhost:3000');
+});
+
