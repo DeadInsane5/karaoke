68027d333df84a99a19479142b3e6f741085e3fd
DeadInsane5 <aditya.ajaykumar.2006@gmail.com>
2025-06-15 18:02:58 +0530

    temp2

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const path = require('path');
 
 // Serve static files (HTML, JS, CSS)
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '')));
 
 app.listen(3000, () => {
   console.log('Server running at http://localhost:3000');
